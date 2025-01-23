a3c9a8a41f290d2bee0e28550505fb3a24b4a169
Romzes732 <mironovaalena431@gmail.com>
2025-01-23 14:33:25 +0700

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,37 @@
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    else:
+        return first
+
+result = get_multiplied_digits(40203)
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#print(result) # Вывод: 24
+
+#result2 = get_multiplied_digits(402030)
+#print(result2) # Вывод: 24
+
+
+
+
+
+
